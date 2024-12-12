cba087bf5b8a4f3f35a4967a5077d863dbffa369
Rifandiysf <rifandiyusuf47@gmail.com>
2024-12-12 20:01:23 +0700

    menammbahkan banner

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,12 @@
+import banner1 from "/Banner.svg"
+
+const Banner = () => {
+  return (
+    <div className="col-span-5 row-span-1 flex justify-left items-center relative rounded-xl bg-primary max-sm:col-span-3 max-sm:row-span-1">
+      <h1 className="ml-8 text-xl font-extrabold text-body max-sm:w-40 max-sm:text-sm max-sm:ml-4">Pahami psikologi untuk mengerti pikiran dan tumbuh bersama</h1>
+      <img src={banner1} className="absolute bottom-0 right-0 max-sm:w-[241px]"/>
+    </div>
+  )
+}
+
+export default Banner
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom"
 import Layout from "../Components/Layout"
-
+import Blob from "/Blob.svg"
+import Banner from "../Components/Banner"
 
 const Dashboard = () => {
   return (
@@ -7,15 +9,18 @@ const Dashboard = () => {
       <Layout/>
       <section className="flex min-h-[800px] ml-[300px] mr-[20px] my-6 max-sm:mx-[10px] max-sm:mb-[80px]">
         <div className="grid w-full grid-cols-5 grid-rows-4 gap-3 max-sm:grid-cols-2 max-sm:grid-rows-6"> 
-          <div className="col-span-5 row-span-1 flex justify-left items-center rounded-xl bg-primary max-sm:col-span-3 max-sm:row-span-1">
-            <h1 className="ml-8 text-xl font-extrabold text-body max-sm:text-sm max-sm:ml-4">Pahami psikologi untuk<br/>mengerti pikiran dan<br/> tumbuh bersama</h1>
-          </div>
+          <Banner/>
+
           <div className="col-span-2 row-span-2 rounded-xl bg-container max-sm:col-span-3 max-sm:row-span-1"></div>
+          
           <div className="col-span-3 row-span-2 rounded-xl bg-container max-sm:col-span-3"></div>
-          <div className="col-span-2 row-span-2 flex flex-col justify-between rounded-xl bg-secondry p-7 max-sm:col-span-3 max-sm:row-span-1">
-            <h1 className="font-semibold text-2xl max-sm:text-base">Cari tahu kondisi atau<br/> kesehatan psikologis kamu<br/> dengan jawab soal-soal<br/> yang kami buat.</h1>
-            <button className="w-full bg-body p-1 rounded-lg font-semibold">Test Personality</button>
+          
+          <div className="col-span-2 row-span-2 flex flex-col justify-between relative rounded-xl bg-secondry p-7 max-sm:col-span-3 max-sm:row-span-1">
+            <img src={Blob} className="absolute right-0 bottom-0 rounded-xl w-[12rem]"/>
+            <h1 className="font-semibold text-xl w-full relative z-1 max-sm:text-base">Cari tahu jenis personality kamu seperti apa dengan jawab soal-soal yang kami buat.</h1>
+            <Link to={'/test-psikologi'} className="text-center w-full bg-body p-1 rounded-lg font-semibold relative z-1">Test Personality</Link>
           </div>
+          
           <div className="col-span-3 row-span-1 rounded-xl bg-container"></div>
         </div>
       </section>
